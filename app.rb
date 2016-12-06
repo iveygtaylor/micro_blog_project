64ace25e21535804e42c02b835656499b44e504d
Ivey Taylor <iveygtaylor@gmail.com>
2016-12-06 12:15:56 -0500

    Adding Rakefile

---
+++ app.rb
@@ -0,0 +1,10 @@
+#app.rb
+require "sinatra"
+require "sinatra/activerecord"
+
+set :database, {adapter: "squlite3", database: "database.db"}
+# # or set :database_file "path/to/database.yml"
+
+class App < Sinatra::Base
+	register Sinatra::ActiveRecordExtension
+end
